93a04891ebaa711fdd31aa2a0fc5027c9672d1ce
ThatsMyName92 <69623155+ThatsMyName92@users.noreply.github.com>
2020-08-13 18:18:13 +0300

    commit

---
+++ Exercise/18.py
@@ -0,0 +1,13 @@
+'''
+Write a Python program to calculate the sum of three given numbers, if the values are equal then return three times of their sum
+'''
+
+n1=input("Please enter 1st number\n")
+n2=input("Please enter 2nd number\n")
+n3=input("Please enter 3rd number\n")
+if
+    n1 == n2 == n3
+print(sum1)
+
+
+
