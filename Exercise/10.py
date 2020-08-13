b1e5b6b5749100439b153493856facdc7677af8f
ThatsMyName92 <69623155+ThatsMyName92@users.noreply.github.com>
2020-08-13 15:06:23 +0300

    up

--- Exercise/10.py
+++ Exercise/10.py
@@ -1,3 +1,10 @@
+''''
+Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn. Go to the editor
+Sample value of n is 5
+Expected Result : 615
+'''
+
+
 num = int(input("Choose a number 1-5:"))
 num1 = int("%s" % (num))
 num2 = int("%s%s" % (num,num))
--- Exercise/8.py
+++ Exercise/8.py
@@ -1,3 +1,10 @@
+'''''
+Write a Python program to display the first and last colors from the following list. Go to the editor
+color_list = ["Red","Green","White" ,"Black"]
+''''
+
+
+
 color_list = ["Red","Green","White" ,"Black"]
 print(color_list[0],"&",color_list[3])
 print( "%s %s"%(color_list[0],color_list[-1]))
