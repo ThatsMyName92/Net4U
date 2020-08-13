f92628cfab3102665eb918b5e4d4623c5b50dbaa
ThatsMyName92 <69623155+ThatsMyName92@users.noreply.github.com>
2020-08-13 19:24:10 +0300

    Lesson

---
+++ Class/Math.py
@@ -0,0 +1,13 @@
+'''
+Alafim=
+Meot=
+Asarot=
+Ahadot=
+'''
+
+num=4567
+Alafim=num//1000
+Meot=num%1000//100
+Asarot=num%100//10
+Ahadot=num%10//1
+print("Alafim: " + str(Alafim) + "\nMeot: " + str(Meot) + "\nAsarot: " + str(Asarot) + "\nAhadot:" + str(Ahadot))
