358563815aa957ee766b4461d5f5fb42f2efad01
ThatsMyName92 <69623155+ThatsMyName92@users.noreply.github.com>
2020-08-30 18:16:35 +0300

    Lesson

--- Class/Market.py
+++ Class/Market.py
@@ -16,15 +16,18 @@ print("\n------------------\nCalculating.....\n------------------")
 sleep(2)
 print("\nSummery of your order:\n----------------------\nTomatos: " + str(tomato) + "\nCucumbers: " + str(cucumber) + "\nCoca-Cola: " + str(cola) + "\nChickens: " + str(chicken) + "\n----------------------")
 
-sum1=tomato*3
-sum2=cucumber*2
-sum3=cola*5
-sum4=chicken*20
-total=sum1+sum2+sum3+sum4
-totvat=total*1.17
+#sum1=tomato*3
+#sum2=cucumber*2
+#sum3=cola*5
+#sum4=chicken*20
+#total=sum1+sum2+sum3+sum4
+#totvat=total*1.17
 sleep(2)
-print("\nYour Bill is: ------ " + str(total) + "$")
-print("Your Bill including VAT: ------ " + str("%.2f" % (totvat)) + "$\n\n----------------------")
+summery=(tomato*3)+(cucumber*2)+(cola*5)+(chicken*20)
+
+print("\nYour Bill is: ------ " + str(summery) + "$")
+print("Your Bill including VAT: ------ " + str("%.2f" % (summery*1.17)) + "$\n\n----------------------")
+
 
 
 
