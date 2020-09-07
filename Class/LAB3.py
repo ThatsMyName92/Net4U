71a10b5295327fd3c074b52107fb33d125df9280
ThatsMyName92 <69623155+ThatsMyName92@users.noreply.github.com>
2020-09-07 17:36:34 +0300

    Lesson

--- Class/LAB3.py
+++ Class/LAB3.py
@@ -4,7 +4,8 @@ a="Iliya Yeriskin"
 b="iliya123@gmail.com"
 c=27
 print("Full name: "+a+"\nEmail: "+b+"\nAge: "+str(c))
-print("Checking if your name is stored in the DB....")
+print("\n\nFull name: "+a[::-1]+"\nAge: "+str(c*3))
+print("\nChecking if your name is stored in the DB....")
 sleep(2)
-d="Yana Idan Yosi Iliya Haim Moshe David"
-print("Iliya" in d)
+DB="Yana Idan Yosi Iliya Haim Moshe David"
+print("Iliya" in DB) #True= Yes in the DB / False= Not in the DB
