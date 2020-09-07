61ef0d35a129f70e5150b735509b1e502ce11a3f
ThatsMyName92 <69623155+ThatsMyName92@users.noreply.github.com>
2020-09-07 17:25:58 +0300

    Lesson

---
+++ Class/LAB3.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+a="Iliya Yeriskin"
+b="iliya123@gmail.com"
+c=27
+print("Full name: "+a+"\nEmail: "+b+"\nAge: "+str(c))
+print("Checking if your name is stored in the DB....")
+sleep(2)
+d="Yana Idan Yosi Iliya Haim Moshe David"
+print("Iliya" in d)
